43494df3b3e76754749ac3557dbf867858c668d2
merkuspb <msemenova21@gmail.com>
2023-05-04 20:22:54 +0300

    prodiver done

--- client/src/types/users/State.ts
+++ client/src/types/users/State.ts
@@ -1,5 +1,11 @@
 import { User } from '../users/User';
+import { Action } from '../Action';
 
 export type State = {
   user: User | undefined;
+};
+
+export type ContextState = {
+  state: State;
+  dispatch: (value: Action) => void;
 };
